04c39f2a8952939851c3a699572a9833aaab240a
Rexraax <thapasurendra567@gmail.com>
2025-02-16 05:24:53 -0800

    Simple Ball Moving Script
    Simple Ball Move Code

--- My project/Assets/Scripts/Ball.cs
+++ My project/Assets/Scripts/Ball.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody Rb;
+    public float speed = 5f; // Speed of movement
+
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check if there is a touch input
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0); // Get the first touch
+
+            if (touch.phase == TouchPhase.Began) // Detect touch start
+            {
+                MoveForward();
+            }
+        }
+    }
+
+    void MoveForward()
+    {
+        // Move the ball forward
+       Rb.linearVelocity = transform.forward * speed;
     }
 }
